0be770f926e2610eac1537acc9c0592cb4a0d9a6
nhochuy154 <nguyenminhhuy2410@gmail.com>
2025-01-27 02:17:47 +0700

    Hoan Thanh Phan Hero Banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
